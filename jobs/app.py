f6ee0216b622a334f0888146bbafcd9adeef44e1
DanChellew <danjchellew@gmail.com>
2019-08-17 17:51:34 +0100

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 from flask import Flask, render_template, g, request, redirect, url_for
 
-PATH = 'db/jobs.sqlite'
+PATH = '/Users/DanChellew/Documents/GitHub/PythonFlask-JobBoard/db/jobs.sqlite'
 
 app = Flask(__name__)
 
